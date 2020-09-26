c6ff72210dc473894cf2fc369bb5653777b6f911
Jordy <0934433@hr.nl>
2020-09-26 23:21:28 +0200

    added Sprint Boot project

---
+++ src/main/java/com/hr/minorfullstackjava/MinorfullstackjavaApplication.java
@@ -0,0 +1,13 @@
+package com.hr.minorfullstackjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinorfullstackjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinorfullstackjavaApplication.class, args);
+    }
+
+}
