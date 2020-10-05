ed7d45a20205decc680164f29abf442009a65e04
DESKTOP-2SG0HB3\Jordy <thiewes24@gmail.com>
2020-10-05 10:42:13 +0200

    '

---
+++ src/main/java/com/hr/minorjava/MinorjavaApplication.java
@@ -0,0 +1,13 @@
+package com.hr.minorjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinorjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinorjavaApplication.class, args);
+	}
+
+}
