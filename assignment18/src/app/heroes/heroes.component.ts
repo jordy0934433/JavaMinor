67d2af49991d96901956de155ce4bdf20c7e69fc
Jordy <34245247+jordy0934433@users.noreply.github.com>
2020-11-15 21:50:41 +0100

    part 4, deel 2

--- assignment18/src/app/heroes/heroes.component.ts
+++ assignment18/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +10,20 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
   selectedHero: Hero;
-  constructor(
-    private heroService: HeroService
-  ) { }
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
   heroes: Hero[];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
   ngOnInit(): void {
     this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: selected hero id ${hero.id}`);
   }
 
   getHeroes(): void {
