65161c22e92b32710b213ad955cb6851d3cd57fe
Jordy <34245247+jordy0934433@users.noreply.github.com>
2020-11-15 17:10:20 +0100

    part 2

--- assignment18/src/app/app.module.ts
+++ assignment18/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
